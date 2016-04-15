db2bd3d23c71de71353fb0bfc9367aeed3febd8f
Jaggeroth <david@hero-id.co.uk>
2016-04-15 19:14:18 +0100

    Add getTableNames function

--- maptool/src/main/java/net/rptools/maptool/client/functions/LookupTableFunction.java
+++ maptool/src/main/java/net/rptools/maptool/client/functions/LookupTableFunction.java
@@ -21,11 +21,12 @@ import net.rptools.maptool.model.LookupTable.LookupEntry;
 import net.rptools.parser.Parser;
 import net.rptools.parser.ParserException;
 import net.rptools.parser.function.AbstractFunction;
+import net.sf.json.JSONArray;
 
 public class LookupTableFunction extends AbstractFunction {
 
 	public LookupTableFunction() {
-		super(1, 3, "tbl", "table", "tblImage", "tableImage");
+		super(0, 3, "tbl", "table", "tblImage", "tableImage", "getTableNames");
 	}
 
 	/** The singleton instance. */
@@ -41,59 +42,74 @@ public class LookupTableFunction extends AbstractFunction {
 
 	@Override
 	public Object childEvaluate(Parser parser, String function, List<Object> params) throws ParserException {
-
-		String name = params.get(0).toString();
-
-		String roll = null;
-		if (params.size() > 1) {
-			roll = params.get(1).toString().length() == 0 ? null : params.get(1).toString();
-		}
-
-		LookupTable lookupTable = MapTool.getCampaign().getLookupTableMap().get(name);
-		if (!MapTool.getPlayer().isGM() && !lookupTable.getAllowLookup()) {
-			if (lookupTable.getVisible()) {
-				throw new ParserException(function + "(): " + I18N.getText("msg.error.tableUnknown") + name);
-			} else {
-				throw new ParserException(function + "(): " + I18N.getText("msg.error.tableAccessProhibited") + ": " + name);
+		
+		if ("getTableNames".equalsIgnoreCase(function)) {
+			String delim = ",";
+			if (params.size() > 0) {
+				delim = params.get(0).toString();
 			}
-		}
-		if (lookupTable == null) {
-			throw new ParserException(I18N.getText("macro.function.LookupTableFunctions.unknownTable", function, name));
-		}
-
-		LookupEntry result = lookupTable.getLookup(roll);
-
-		if (function.equals("table") || function.equals("tbl")) {
-			String val = result.getValue();
-			try {
-				BigDecimal bival = new BigDecimal(val);
-				return bival;
-			} catch (NumberFormatException nfe) {
-				return val;
+			if ("json".equalsIgnoreCase(delim))
+				return JSONArray.fromObject(MapTool.getCampaign().getLookupTableMap().keySet());
+			String results = "";
+			for (LookupTable lt :MapTool.getCampaign().getLookupTableMap().values()) {
+				results=results+(results!=""?delim:"")+lt.getName();
 			}
-		} else { // We want the image URI
+			return results;
+		} else {
 
-			if (result.getImageId() == null) {
-				throw new ParserException(I18N.getText("macro.function.LookupTableFunctions.noImage", function, name));
+			String name = params.get(0).toString();
+
+			String roll = null;
+			if (params.size() > 1) {
+				roll = params.get(1).toString().length() == 0 ? null : params.get(1).toString();
 			}
 
-			BigDecimal size = null;
-			if (params.size() > 2) {
-				if (params.get(2) instanceof BigDecimal) {
-					size = (BigDecimal) params.get(2);
+			LookupTable lookupTable = MapTool.getCampaign().getLookupTableMap().get(name);
+			if (!MapTool.getPlayer().isGM() && !lookupTable.getAllowLookup()) {
+				if (lookupTable.getVisible()) {
+					throw new ParserException(function + "(): " + I18N.getText("msg.error.tableUnknown") + name);
 				} else {
-					throw new ParserException(I18N.getText("macro.function.LookupTableFunctions.invalidSize", function));
+					throw new ParserException(function + "(): " + I18N.getText("msg.error.tableAccessProhibited") + ": " + name);
 				}
 			}
-
-			StringBuilder assetId = new StringBuilder("asset://");
-			assetId.append(result.getImageId().toString());
-			if (size != null) {
-				int i = Math.max(size.intValue(), 1); // Constrain to a minimum of 1
-				assetId.append("-");
-				assetId.append(i);
+			if (lookupTable == null) {
+				throw new ParserException(I18N.getText("macro.function.LookupTableFunctions.unknownTable", function, name));
 			}
-			return assetId.toString();
+
+			LookupEntry result = lookupTable.getLookup(roll);
+
+			if (function.equals("table") || function.equals("tbl")) {
+				String val = result.getValue();
+				try {
+					BigDecimal bival = new BigDecimal(val);
+					return bival;
+				} catch (NumberFormatException nfe) {
+					return val;
+				}
+			} else { // We want the image URI
+
+				if (result.getImageId() == null) {
+					throw new ParserException(I18N.getText("macro.function.LookupTableFunctions.noImage", function, name));
+				}
+
+				BigDecimal size = null;
+				if (params.size() > 2) {
+					if (params.get(2) instanceof BigDecimal) {
+						size = (BigDecimal) params.get(2);
+					} else {
+						throw new ParserException(I18N.getText("macro.function.LookupTableFunctions.invalidSize", function));
+					}
+				}
+
+				StringBuilder assetId = new StringBuilder("asset://");
+				assetId.append(result.getImageId().toString());
+				if (size != null) {
+					int i = Math.max(size.intValue(), 1); // Constrain to a minimum of 1
+					assetId.append("-");
+					assetId.append(i);
+				}
+				return assetId.toString();
+			}
 		}
 	}
 }
