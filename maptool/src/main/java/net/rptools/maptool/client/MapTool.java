c338dea7c8b6cf3edf2d4c0b4dfdef8fc9821561
Craig Wisniewski <cwisniew@mewmpc026.wm.corp.local>
2016-05-09 17:53:20 +0930

    Fix for trailing characters on end of version string

--- maptool/src/main/java/net/rptools/maptool/client/MapTool.java
+++ maptool/src/main/java/net/rptools/maptool/client/MapTool.java
@@ -658,7 +658,7 @@ public class MapTool {
 			version = "DEVELOPMENT";
 			try {
 				if (MapTool.class.getClassLoader().getResource(VERSION_TXT) != null) {
-					version = new String(FileUtil.loadResource(VERSION_TXT));
+					version = new String(FileUtil.loadResource(VERSION_TXT)).trim();
 				}
 			} catch (IOException ioe) {
 				String msg = I18N.getText("msg.info.versionFile", VERSION_TXT);
