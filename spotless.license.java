7ab15a3ec899351ddb126686e85af4ed92de962e
Frank Edwards <azhreifje@gmail.com>
2016-06-06 19:35:51 -0400

    Updated license text; spirit remains the same
    The doc/LICENSE file was not specific enough about the use of
    the GNU Affero GPL and tried to provide a summary of the various
    libraries covered by the Apache or MIT licenses.  The summary is
    removed and references directly to the READMEs for each library
    inserted instead.

--- spotless.license.java
+++ spotless.license.java
@@ -1,12 +1,14 @@
 /*
- * This software Copyright by the RPTools.net development team, and licensed
- * under the GPL Version 3 or, at your option, any later version.
+ * This software Copyright by the RPTools.net development team, and
+ * licensed under the Affero GPL Version 3 or, at your option, any later
+ * version.
  *
- * MapTool 2 Source Code is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * MapTool Source Code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU General Public License along with
- * this source Code. If not, see <http://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU Affero General Public
+ * License * along with this source Code.  If not, please visit
+ * <http://www.gnu.org/licenses/> and specifically the Affero license
+ * text at <http://www.gnu.org/licenses/agpl.html>.
  */
