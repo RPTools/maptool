a167e19bf1ac5d488b32ae01d127c1d32e175d54
Frank Edwards <azhreifje@gmail.com>
2018-02-08 23:43:08 -0500

    YAT

--- .travis/installer.sh
+++ .travis/installer.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Flag any non-zero exit status as script failure and abort!
 set -ev
+
+# If there is no version, FAIL!
+VERSION=${VERSION:?"Must set 'VERSION' in .travis.yml!"}
+
 case "$1" in
+    # For macOS, we have to download JDK9 and install it.
     osx)    COOKIE1="gpw_e24=http%3A%2F%2Fwww.oracle.com%2F"
             COOKIE2="oraclelicense=accept-securebackup-cookie"
             BASEURL=http://download.oracle.com/otn-pub/java/jdk
@@ -14,4 +20,12 @@ case "$1" in
             echo "Completed JDK installation..."
             ;;
 esac
-exit 0
+# For both systems, we have to download the latest Gradle 4.x
+# because older versions couldn't parse '9.0.4' as a Java version
+# string.
+
+# Download desired version of Gradle
+wget https://services.gradle.org/distributions/gradle-$VERSION-bin.zip
+
+# Unpack it into the current directory
+unzip -qq gradle-$VERSION-bin.zip
