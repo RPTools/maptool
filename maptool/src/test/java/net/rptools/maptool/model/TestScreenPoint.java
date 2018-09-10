ca843636ce6674ebf29faf036b3b312228366b57
Frank Edwards <azhreifje@gmail.com>
2018-09-09 20:10:07 -0400

    Modified test for headless environment
    The TestScreenPoint test relied on creating a ZoneRenderer, but they
    can't be created in a headless environment (it depends on
    Component.getDropTarget(), which Swing doesn't set when headless).

--- maptool/src/test/java/net/rptools/maptool/model/TestScreenPoint.java
+++ maptool/src/test/java/net/rptools/maptool/model/TestScreenPoint.java
@@ -13,6 +13,8 @@
  */
 package net.rptools.maptool.model;
 
+import java.awt.GraphicsEnvironment;
+
 import junit.framework.TestCase;
 import net.rptools.maptool.client.ScreenPoint;
 import net.rptools.maptool.client.ui.zone.ZoneRenderer;
@@ -20,13 +22,15 @@ import net.rptools.maptool.client.ui.zone.ZoneRendererFactory;
 
 public class TestScreenPoint extends TestCase {
 	public void testConversion() throws Exception {
-		ZoneRenderer renderer = ZoneRendererFactory.newRenderer(new Zone());
-		renderer.moveViewBy(-100, -100);
+		if (!GraphicsEnvironment.isHeadless()) {
+			ZoneRenderer renderer = ZoneRendererFactory.newRenderer(new Zone());
+			renderer.moveViewBy(-100, -100);
 
-		for (int i = -10; i < 10; i++) {
-			for (int j = -10; j < 10; j++) {
-				ZonePoint zp = new ZonePoint(i, j);
-				assertEquals(zp, ScreenPoint.fromZonePoint(renderer, zp).convertToZone(renderer));
+			for (int i = -10; i < 10; i++) {
+				for (int j = -10; j < 10; j++) {
+					ZonePoint zp = new ZonePoint(i, j);
+					assertEquals(zp, ScreenPoint.fromZonePoint(renderer, zp).convertToZone(renderer));
+				}
 			}
 		}
 	}
