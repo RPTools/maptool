c6a5667beae06b647f33aaaa8d7b29c3fe3f1bf0
I368438 <lee_foster@bcbsil.com>
2018-10-04 17:33:19 -0500

    WIP - Run MapTool with OpenJDK 11 + OpenJFX
     * gradle run now launches working Hello World!

--- src/main/java/net/rptools/maptool_fx/MapTool.java
+++ src/main/java/net/rptools/maptool_fx/MapTool.java
@@ -9,16 +9,24 @@
 package net.rptools.maptool_fx;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MapTool extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(l, 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 	}
 
 	public static void main(String[] args) {
+		System.out.println("BAM!");
 		launch(args);
 	}
 }
