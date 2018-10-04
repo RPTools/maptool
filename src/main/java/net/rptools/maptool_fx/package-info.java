c5ab5d1e9b3da4f5ec28caa8407d23d9a7ab31b9
I368438 <lee_foster@bcbsil.com>
2018-10-04 16:39:05 -0500

    WIP - Run MapTool with OpenJDK 11 + OpenJFX

---
+++ src/main/java/net/rptools/maptool_fx/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Current home for new classes for MapTool 2.0.
+ * <p>
+ * This is purely to keep things organized and easy to find for now.
+ *
+ * @since 2.0
+ * @author Jamz
+ * @version 2.0
+ */
+package net.rptools.maptool_fx;
