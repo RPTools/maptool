7f34c25ab28b7d258434792fdec23ad3453274ac
Darinth <cerka@marshall.edu>
2019-01-07 14:14:59 -0500

    Players nolonger get "Send to..." options on GM notes I happened to be looking at EditTokenDialog.java and remembered this bug. Quick to fix.

--- maptool/src/main/java/net/rptools/maptool/client/tool/PointerTool.java
+++ maptool/src/main/java/net/rptools/maptool/client/tool/PointerTool.java
@@ -1395,7 +1395,7 @@ public class PointerTool extends DefaultTool implements ZoneOverlay {
 										propName = property.getShortName();
 									}
 									Object value = tokenUnderMouse.getEvaluatedProperty(resolver, property.getName());
-                  resolver.flush();
+									resolver.flush();
 									propertyMap.put(propName, value != null ? value.toString() : "");
 								}
 							}
