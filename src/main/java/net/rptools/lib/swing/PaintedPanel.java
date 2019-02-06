e3fd0933ea509b033dca4313ed3891c6e8526798
UOB\isdsr <d@br>
2019-02-06 13:57:25 +0000

    New icon for transparent colour

--- src/main/java/net/rptools/lib/swing/PaintedPanel.java
+++ src/main/java/net/rptools/lib/swing/PaintedPanel.java
@@ -13,7 +13,12 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -51,10 +56,19 @@ public class PaintedPanel extends JPanel {
 			((Graphics2D) g).setPaint(paint);
 			g.fillRect(0, 0, size.width, size.height);
 		} else {
-			g.setColor(Color.white);
-			g.fillRect(0, 0, size.width, size.height);
-			g.setColor(Color.red);
-			g.drawLine(size.width - 1, 0, 0, size.height - 1);
+      try {
+        BufferedImage texture;
+        texture = ImageIO.read(getClass().getResource("/net/rptools/lib/image/icons/transparent.png"));
+        TexturePaint tp = new TexturePaint(texture, new Rectangle(0, 0, 28, 28));
+        ((Graphics2D) g).setPaint(tp);
+        g.fillRect(0, 0, size.width, size.height);
+      } catch (IOException e) {
+        System.out.println(e.getMessage());
+        g.setColor(Color.white);
+        g.fillRect(0, 0, size.width, size.height);
+        g.setColor(Color.red);
+        g.drawLine(size.width - 1, 0, 0, size.height - 1);
+      }
 		}
 	}
 }
