277757ca30caeda522590a95f4afbfab3344a44a
Craig Wisniewski <craig.wisniewski@gmail.com>
2020-01-10 11:24:21 +1030

    Spotless Apply

--- src/main/java/net/rptools/maptool/client/macro/impl/VersionMacro.java
+++ src/main/java/net/rptools/maptool/client/macro/impl/VersionMacro.java
@@ -1,3 +1,17 @@
+/*
+ * This software Copyright by the RPTools.net development team, and
+ * licensed under the Affero GPL Version 3 or, at your option, any later
+ * version.
+ *
+ * MapTool Source Code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License * along with this source Code.  If not, please visit
+ * <http://www.gnu.org/licenses/> and specifically the Affero license
+ * text at <http://www.gnu.org/licenses/agpl.html>.
+ */
 package net.rptools.maptool.client.macro.impl;
 
 import net.rptools.maptool.client.MapTool;
@@ -12,9 +26,7 @@ import net.rptools.maptool.language.I18N;
     aliases = {"v"},
     description = "slashversion.description")
 
-/**
- * This class represents the /version command run from the chat panel.
- */
+/** This class represents the /version command run from the chat panel. */
 public class VersionMacro implements Macro {
 
   @Override
