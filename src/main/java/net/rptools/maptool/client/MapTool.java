5865015139d5256ea22e9ce8b2ea842d9c2abf92
Jamz <jamz@nerps.net>
2020-04-10 17:30:21 -0500

    Initial work to convert over to open jdk 14
    Signed-off-by: JamzTheMan <JamzTheMan@gmail.com>

--- src/main/java/net/rptools/maptool/client/MapTool.java
+++ src/main/java/net/rptools/maptool/client/MapTool.java
@@ -15,8 +15,6 @@
 package net.rptools.maptool.client;
 
 import com.jidesoft.plaf.LookAndFeelFactory;
-import com.jidesoft.plaf.UIDefaultsLookup;
-import com.jidesoft.plaf.basic.ThemePainter;
 import de.muntjak.tinylookandfeel.Theme;
 import de.muntjak.tinylookandfeel.util.SBReference;
 import io.sentry.Sentry;
@@ -1255,7 +1253,7 @@ public class MapTool {
         new LookAndFeelFactory.UIDefaultsCustomizer() {
           @Override
           public void customize(UIDefaults defaults) {
-            ThemePainter painter = (ThemePainter) UIDefaultsLookup.get("Theme.painter");
+//            ThemePainter painter = (ThemePainter) UIDefaultsLookup.get("Theme.painter");
             defaults.put("OptionPaneUI", "com.jidesoft.plaf.basic.BasicJideOptionPaneUI");
 
             defaults.put("OptionPane.showBanner", Boolean.TRUE); // show banner or not. default
@@ -1269,9 +1267,9 @@ public class MapTool {
             defaults.put("OptionPane.bannerFontSize", 13);
             defaults.put("OptionPane.bannerFontStyle", Font.BOLD);
             defaults.put("OptionPane.bannerMaxCharsPerLine", 60);
-            defaults.put(
-                "OptionPane.bannerForeground",
-                painter != null ? painter.getOptionPaneBannerForeground() : null); // you
+//            defaults.put(
+//                "OptionPane.bannerForeground",
+//                painter != null ? painter.getOptionPaneBannerForeground() : null); // you
             // should
             // adjust
             // this
@@ -1287,12 +1285,12 @@ public class MapTool {
 
             // set both bannerBackgroundDk and bannerBackgroundLt to null if you don't want
             // gradient
-            defaults.put(
-                "OptionPane.bannerBackgroundDk",
-                painter != null ? painter.getOptionPaneBannerDk() : null);
-            defaults.put(
-                "OptionPane.bannerBackgroundLt",
-                painter != null ? painter.getOptionPaneBannerLt() : null);
+//            defaults.put(
+//                "OptionPane.bannerBackgroundDk",
+//                painter != null ? painter.getOptionPaneBannerDk() : null);
+//            defaults.put(
+//                "OptionPane.bannerBackgroundLt",
+//                painter != null ? painter.getOptionPaneBannerLt() : null);
             defaults.put("OptionPane.bannerBackgroundDirection", Boolean.TRUE); // default is
             // true
 
