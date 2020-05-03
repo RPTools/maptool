04b9ce45f8e7088c4095c4aeecb884dce46c292a
JamzTheMan <JamzTheMan@gmail.com>
2020-05-03 11:52:31 -0500

    JDK 14 Package updates
     * Add new gradle prop `developerRelease` which will append to installer image/app name to better run alpha/beta releases side by side production release
       * TODO: use git tag info to create this property on Releases
     * Refactored UserJvmPrefs.java to UserJvmOptions which now directly modifies the .cfg used to launch MapTool via installed packages
       * Warning: This is obviously dangerous as incorrect information can prevent MapTool from starting but it's the only solution we have at the momement
     * Cleaned up the PreferencesDialog
       * TODO: tabbedPane.setEnabledAt(tabbedPane.indexOfTab("Startup"), false); has hard coded English, use i18n?
     * New Dev icon now set on developerRelease
     * .cmpgn file associations added
     * Other cleanup...
    
    Signed-off-by: JamzTheMan <JamzTheMan@gmail.com>

--- src/main/java/net/rptools/maptool/client/MapTool.java
+++ src/main/java/net/rptools/maptool/client/MapTool.java
@@ -96,7 +96,7 @@ import net.rptools.maptool.server.ServerPolicy;
 import net.rptools.maptool.transfer.AssetTransferManager;
 import net.rptools.maptool.util.StringUtil;
 import net.rptools.maptool.util.UPnPUtil;
-import net.rptools.maptool.util.UserJvmPrefs;
+import net.rptools.maptool.util.UserJvmOptions;
 import net.rptools.maptool.webapi.MTWebAppServer;
 import net.tsc.servicediscovery.ServiceAnnouncer;
 import org.apache.commons.cli.CommandLine;
@@ -1608,7 +1608,7 @@ public class MapTool {
       listMacros = getCommandLineOption(cmd, "macros");
 
       if (getCommandLineOption(cmd, "reset")) {
-        UserJvmPrefs.resetJvmOptions();
+        UserJvmOptions.resetJvmOptions();
       }
     } catch (ParseException e) {
       // MapTool.showWarning() can be invoked here.  It will log the stacktrace,
