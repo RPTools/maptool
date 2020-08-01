d986749920fd6db14d8119e1e0929963a72236dc
Eric Budai <eric_budai@hotmail.com>
2020-08-01 19:33:32 -0400

    unnecessary string conversions #2135

--- src/main/java/net/rptools/maptool/client/ui/tokenpanel/InitiativePanel.java
+++ src/main/java/net/rptools/maptool/client/ui/tokenpanel/InitiativePanel.java
@@ -278,7 +278,7 @@ public class InitiativePanel extends JPanel
 
   private void updateRound() {
     if (list.getRound() > 0)
-      round.setText(I18N.getText("initPanel.round") + " " + Integer.toString(list.getRound()));
+      round.setText(I18N.getText("initPanel.round") + " " + list.getRound());
     else round.setText("");
   }
 
