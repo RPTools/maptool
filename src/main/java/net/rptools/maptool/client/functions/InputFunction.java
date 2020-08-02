c5d952ba7d2f8419be2331fca0a6800d0833a6f1
Eric Budai <eric_budai@hotmail.com>
2020-08-02 09:11:56 -0400

    string concatenation as parameter to StringBuilder.append() #2135

--- src/main/java/net/rptools/maptool/client/functions/InputFunction.java
+++ src/main/java/net/rptools/maptool/client/functions/InputFunction.java
@@ -1220,7 +1220,7 @@ public class InputFunction extends AbstractFunction {
           } else {
             resolver.setVariable(vs.name, newValue.trim());
           }
-          allAssignments.append(vs.name + "=" + newValue.trim() + " ## ");
+          allAssignments.append(vs.name).append("=").append(newValue.trim()).append(" ## ");
         }
       }
       if (cp.tabVarSpec != null) {
