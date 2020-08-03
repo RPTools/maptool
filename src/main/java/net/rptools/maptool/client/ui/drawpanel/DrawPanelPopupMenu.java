7a56e7bee8c3746167a1ba65d979264fc4583a48
Eric Budai <eric_budai@hotmail.com>
2020-08-03 07:40:41 -0400

    null check inserted #2046

--- src/main/java/net/rptools/maptool/client/ui/drawpanel/DrawPanelPopupMenu.java
+++ src/main/java/net/rptools/maptool/client/ui/drawpanel/DrawPanelPopupMenu.java
@@ -403,8 +403,8 @@ public class DrawPanelPopupMenu extends JPopupMenu {
       List<DrawnElement> drawableList = renderer.getZone().getAllDrawnElements();
       for (GUID guid : selectedDrawSet) {
         DrawnElement de = findDrawnElement(drawableList, guid);
-        if (de.getDrawable() instanceof AbstractTemplate) continue;
-        if (de != null) VblTool(de.getDrawable(), pathOnly, isEraser);
+        if (de == null || de.getDrawable() instanceof AbstractTemplate) continue;
+        VblTool(de.getDrawable(), pathOnly, isEraser);
       }
     }
   }
