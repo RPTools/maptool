d40dac692345c02faf65ac457f9fade523a18944
MeloDub <andrea.melosi2@gmail.com>
2021-03-13 15:34:36 +0100

    #2349 add shortcut key Shift+F to flip selected tokens vertically.

--- src/main/java/net/rptools/maptool/client/tool/PointerTool.java
+++ src/main/java/net/rptools/maptool/client/tool/PointerTool.java
@@ -1307,7 +1307,8 @@ public class PointerTool extends DefaultTool {
             }
           }
         });
-    actionMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_F, 0), new FlipTokenActionListener());
+    actionMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_F, 0), new FlipTokenHorizontalActionListener());
+    actionMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.SHIFT_DOWN_MASK), new FlipTokenVerticalActionListener());
   }
 
   /**
@@ -1496,7 +1497,7 @@ public class PointerTool extends DefaultTool {
     }
   }
 
-  private class FlipTokenActionListener extends AbstractAction {
+  private class FlipTokenHorizontalActionListener extends AbstractAction {
     private static final long serialVersionUID = -6286351028470892136L;
 
     @Override
@@ -1507,6 +1508,22 @@ public class PointerTool extends DefaultTool {
           continue;
         }
         MapTool.serverCommand().updateTokenProperty(token, Token.Update.flipX);
+      }
+      MapTool.getFrame().refresh();
+    }
+  }
+
+  private class FlipTokenVerticalActionListener extends AbstractAction {
+    private static final long serialVersionUID = -6286351028470892137L;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      List<Token> selectedTokensList = renderer.getSelectedTokensList();
+      for (Token token : selectedTokensList) {
+        if (token == null) {
+          continue;
+        }
+        MapTool.serverCommand().updateTokenProperty(token, Token.Update.flipY);
       }
       MapTool.getFrame().refresh();
     }
