2d613f1bdf9399e7f152d9518ec353e9e9c6a439
Phergus <34379254+Phergus@users.noreply.github.com>
2021-07-03 16:30:04 -0600

    Read light properties from UVTT file
    And then store them in the GM notes of the light token.

--- src/main/java/net/rptools/maptool/client/utilities/DungeonDraftImporter.java
+++ src/main/java/net/rptools/maptool/client/utilities/DungeonDraftImporter.java
@@ -27,6 +27,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
 import net.rptools.maptool.client.AppStyle;
 import net.rptools.maptool.client.MapTool;
 import net.rptools.maptool.client.ui.MapPropertiesDialog;
@@ -232,6 +234,14 @@ public class DungeonDraftImporter {
 
         lightToken.setX((int) (position.get("x").getAsDouble() * pixelsPerCell) - LIGHT_WIDTH / 2);
         lightToken.setY((int) (position.get("y").getAsDouble() * pixelsPerCell) - LIGHT_HEIGHT / 2);
+
+        JsonObject lightValues = new JsonObject();
+        lightValues.addProperty("range", ele.getAsJsonObject().getAsJsonPrimitive("range").getAsBigDecimal());
+        lightValues.addProperty("intensity", ele.getAsJsonObject().getAsJsonPrimitive("intensity").getAsBigDecimal());
+        lightValues.addProperty("color", ele.getAsJsonObject().getAsJsonPrimitive("color").getAsString());
+        lightValues.addProperty("shadows", ele.getAsJsonObject().getAsJsonPrimitive("shadows").getAsBoolean()? BigDecimal.ONE : BigDecimal.ZERO);
+        lightToken.setGMNotes(lightValues.toString());
+
         zone.putToken(lightToken);
         lightNo++;
       } else {
