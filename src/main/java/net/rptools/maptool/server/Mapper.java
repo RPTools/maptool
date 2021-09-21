aa1b01fadb7414a83ea108aa8be39141303879a5
Thomas Kunze <thomas.kunze@gmx.com>
2021-09-21 20:56:05 +0200

    tested addTopology

--- src/main/java/net/rptools/maptool/server/Mapper.java
+++ src/main/java/net/rptools/maptool/server/Mapper.java
@@ -71,111 +71,113 @@ public class Mapper {
   public static Area map(AreaDto areaDto) {
     var segmentIterator = areaDto.getSegmentsList().iterator();
 
-    var it = new PathIterator() {
-      private SegmentDto currentSegment;
-      @Override
-      public int getWindingRule() {
-        return areaDto.getWindingValue();
-      }
+    var it =
+        new PathIterator() {
+          private SegmentDto currentSegment = segmentIterator.next();
 
-      @Override
-      public boolean isDone() {
-        return !segmentIterator.hasNext();
-      }
+          @Override
+          public int getWindingRule() {
+            return areaDto.getWindingValue();
+          }
 
-      @Override
-      public void next() {
-        currentSegment = segmentIterator.next();
-      }
+          @Override
+          public boolean isDone() {
+            return !segmentIterator.hasNext();
+          }
 
-      @Override
-      public int currentSegment(float[] coords) {
-        switch (currentSegment.getSegmentTypeCase()){
-          case MOVE_TO -> {
-            var segment = currentSegment.getMoveTo();
-            var point0 = segment.getPoint0();
-            coords[0] = (float)point0.getX();
-            coords[1] = (float)point0.getY();
-            return PathIterator.SEG_MOVETO;
+          @Override
+          public void next() {
+            currentSegment = segmentIterator.next();
           }
-          case LINE_TO -> {
-            var segment = currentSegment.getLineTo();
-            var point0 = segment.getPoint0();
-            coords[0] = (float)point0.getX();
-            coords[1] = (float)point0.getY();
-            return PathIterator.SEG_LINETO;
-          }
-          case QUAD_TO -> {
-            var segment = currentSegment.getQuadTo();
-            var point0 = segment.getPoint0();
-            coords[0] = (float)point0.getX();
-            coords[1] = (float)point0.getY();
-            var point1 = segment.getPoint1();
-            coords[2] = (float)point1.getX();
-            coords[3] = (float)point1.getY();
-            return PathIterator.SEG_QUADTO;
-          }
-          case CUBIC_TO -> {
-            var segment = currentSegment.getCubicTo();
-            var point0 = segment.getPoint0();
-            coords[0] = (float)point0.getX();
-            coords[1] = (float)point0.getY();
-            var point1 = segment.getPoint1();
-            coords[2] = (float)point1.getX();
-            coords[3] = (float)point1.getY();
-            var point2 = segment.getPoint2();
-            coords[4] = (float)point2.getX();
-            coords[5] = (float)point2.getY();
-            return PathIterator.SEG_CUBICTO;
-          }
-        }
-        return SEG_CLOSE;
-      }
 
-      @Override
-      public int currentSegment(double[] coords) {
-        switch (currentSegment.getSegmentTypeCase()){
-          case MOVE_TO -> {
-            var segment = currentSegment.getMoveTo();
-            var point0 = segment.getPoint0();
-            coords[0] = point0.getX();
-            coords[1] = point0.getY();
-            return PathIterator.SEG_MOVETO;
+          @Override
+          public int currentSegment(float[] coords) {
+            switch (currentSegment.getSegmentTypeCase()) {
+              case MOVE_TO -> {
+                var segment = currentSegment.getMoveTo();
+                var point0 = segment.getPoint0();
+                coords[0] = (float) point0.getX();
+                coords[1] = (float) point0.getY();
+                return PathIterator.SEG_MOVETO;
+              }
+              case LINE_TO -> {
+                var segment = currentSegment.getLineTo();
+                var point0 = segment.getPoint0();
+                coords[0] = (float) point0.getX();
+                coords[1] = (float) point0.getY();
+                return PathIterator.SEG_LINETO;
+              }
+              case QUAD_TO -> {
+                var segment = currentSegment.getQuadTo();
+                var point0 = segment.getPoint0();
+                coords[0] = (float) point0.getX();
+                coords[1] = (float) point0.getY();
+                var point1 = segment.getPoint1();
+                coords[2] = (float) point1.getX();
+                coords[3] = (float) point1.getY();
+                return PathIterator.SEG_QUADTO;
+              }
+              case CUBIC_TO -> {
+                var segment = currentSegment.getCubicTo();
+                var point0 = segment.getPoint0();
+                coords[0] = (float) point0.getX();
+                coords[1] = (float) point0.getY();
+                var point1 = segment.getPoint1();
+                coords[2] = (float) point1.getX();
+                coords[3] = (float) point1.getY();
+                var point2 = segment.getPoint2();
+                coords[4] = (float) point2.getX();
+                coords[5] = (float) point2.getY();
+                return PathIterator.SEG_CUBICTO;
+              }
+            }
+            return SEG_CLOSE;
           }
-          case LINE_TO -> {
-            var segment = currentSegment.getLineTo();
-            var point0 = segment.getPoint0();
-            coords[0] = point0.getX();
-            coords[1] = point0.getY();
-            return PathIterator.SEG_LINETO;
+
+          @Override
+          public int currentSegment(double[] coords) {
+            switch (currentSegment.getSegmentTypeCase()) {
+              case MOVE_TO -> {
+                var segment = currentSegment.getMoveTo();
+                var point0 = segment.getPoint0();
+                coords[0] = point0.getX();
+                coords[1] = point0.getY();
+                return PathIterator.SEG_MOVETO;
+              }
+              case LINE_TO -> {
+                var segment = currentSegment.getLineTo();
+                var point0 = segment.getPoint0();
+                coords[0] = point0.getX();
+                coords[1] = point0.getY();
+                return PathIterator.SEG_LINETO;
+              }
+              case QUAD_TO -> {
+                var segment = currentSegment.getQuadTo();
+                var point0 = segment.getPoint0();
+                coords[0] = point0.getX();
+                coords[1] = point0.getY();
+                var point1 = segment.getPoint1();
+                coords[2] = point1.getX();
+                coords[3] = point1.getY();
+                return PathIterator.SEG_QUADTO;
+              }
+              case CUBIC_TO -> {
+                var segment = currentSegment.getCubicTo();
+                var point0 = segment.getPoint0();
+                coords[0] = point0.getX();
+                coords[1] = point0.getY();
+                var point1 = segment.getPoint1();
+                coords[2] = point1.getX();
+                coords[3] = point1.getY();
+                var point2 = segment.getPoint2();
+                coords[4] = point2.getX();
+                coords[5] = point2.getY();
+                return PathIterator.SEG_CUBICTO;
+              }
+            }
+            return SEG_CLOSE;
           }
-          case QUAD_TO -> {
-            var segment = currentSegment.getQuadTo();
-            var point0 = segment.getPoint0();
-            coords[0] = point0.getX();
-            coords[1] = point0.getY();
-            var point1 = segment.getPoint1();
-            coords[2] = point1.getX();
-            coords[3] = point1.getY();
-            return PathIterator.SEG_QUADTO;
-          }
-          case CUBIC_TO -> {
-            var segment = currentSegment.getCubicTo();
-            var point0 = segment.getPoint0();
-            coords[0] = point0.getX();
-            coords[1] = point0.getY();
-            var point1 = segment.getPoint1();
-            coords[2] = point1.getX();
-            coords[3] = point1.getY();
-            var point2 = segment.getPoint2();
-            coords[4] = point2.getX();
-            coords[5] = point2.getY();
-            return PathIterator.SEG_CUBICTO;
-          }
-        }
-        return SEG_CLOSE;
-      }
-    };
+        };
     var path = new Path2D.Float();
     path.append(it, false);
     return new Area(path);
@@ -204,19 +206,18 @@ public class Mapper {
         case PathIterator.SEG_QUADTO -> {
           var point0Builder = DoublePointDto.newBuilder().setX(floats[0]).setY(floats[1]);
           var point1Builder = DoublePointDto.newBuilder().setX(floats[2]).setY(floats[3]);
-          var quadTo = QuadToSegment.newBuilder()
-              .setPoint0(point0Builder)
-              .setPoint1(point1Builder);
+          var quadTo = QuadToSegment.newBuilder().setPoint0(point0Builder).setPoint1(point1Builder);
           segmentBuilder.setQuadTo(quadTo);
         }
         case PathIterator.SEG_CUBICTO -> {
           var point0Builder = DoublePointDto.newBuilder().setX(floats[0]).setY(floats[1]);
           var point1Builder = DoublePointDto.newBuilder().setX(floats[2]).setY(floats[3]);
           var point2Builder = DoublePointDto.newBuilder().setX(floats[4]).setY(floats[5]);
-          var cubicTo = CubicToSegment.newBuilder()
-              .setPoint0(point0Builder)
-              .setPoint1(point1Builder)
-              .setPoint2(point2Builder);
+          var cubicTo =
+              CubicToSegment.newBuilder()
+                  .setPoint0(point0Builder)
+                  .setPoint1(point1Builder)
+                  .setPoint2(point2Builder);
           segmentBuilder.setCubicTo(cubicTo);
         }
         case PathIterator.SEG_CLOSE -> segmentBuilder.setClose(CloseSegment.newBuilder());
