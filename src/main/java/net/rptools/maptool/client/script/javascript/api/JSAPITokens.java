485e1f96c04fd3f64f8fbf2504a9b0caeb4bc491
Logan Perkins <logan@git.lp-programming.com>
2021-10-06 17:59:21 -0700

    Remove extra helper functions from unstable token-finding API

--- src/main/java/net/rptools/maptool/client/script/javascript/api/JSAPITokens.java
+++ src/main/java/net/rptools/maptool/client/script/javascript/api/JSAPITokens.java
@@ -19,49 +19,13 @@ import java.util.List;
 import net.rptools.maptool.client.MapTool;
 import net.rptools.maptool.client.script.javascript.*;
 import net.rptools.maptool.client.ui.zone.ZoneRenderer;
-import net.rptools.maptool.language.I18N;
 import net.rptools.maptool.model.Token;
-import net.rptools.parser.ParserException;
 import org.graalvm.polyglot.HostAccess;
 
 public class JSAPITokens implements MapToolJSAPIInterface {
   @Override
   public String serializeToString() {
     return "MapTool.tokens";
-  }
-
-  @HostAccess.Export
-  public List<Object> getAllTokens() {
-    final List<Object> tokens = new ArrayList<>();
-    boolean trusted = JSScriptEngine.inTrustedContext();
-    if (!trusted) {
-      MapTool.getFrame()
-          .getZoneRenderers()
-          .forEach(
-              (z -> {
-                if (z.getZone().isVisible()) {
-                  z.getVisibleTokens()
-                      .forEach(
-                          (t -> {
-                            tokens.add(new JSAPIToken(t));
-                          }));
-                }
-              }));
-
-    } else {
-      MapTool.getFrame()
-          .getZoneRenderers()
-          .forEach(
-              (z -> {
-                z.getZone()
-                    .getTokens()
-                    .forEach(
-                        (t -> {
-                          tokens.add(new JSAPIToken(t));
-                        }));
-              }));
-    }
-    return tokens;
   }
 
   @HostAccess.Export
@@ -92,41 +56,6 @@ public class JSAPITokens implements MapToolJSAPIInterface {
   }
 
   @HostAccess.Export
-  public List<Object> getOwnedTokens() throws ParserException {
-    String playerId = MapTool.getPlayer().getName();
-    return getOwnedTokens(playerId);
-  }
-
-  @HostAccess.Export
-  public List<Object> getOwnedTokens(String playerId) throws ParserException {
-    final List<Object> tokens = new ArrayList<>();
-    boolean trusted = JSScriptEngine.inTrustedContext();
-    if (!trusted && !(MapTool.getPlayer().getName().equals(playerId))) {
-      throw new ParserException(
-          I18N.getText(
-              "macro.function.initiative.gmOrOwner",
-              "getOwnedTokens with a different player name"));
-    }
-    MapTool.getFrame()
-        .getZoneRenderers()
-        .forEach(
-            (z -> {
-              if (trusted || z.getZone().isVisible()) {
-                z.getZone()
-                    .getTokens()
-                    .forEach(
-                        (t -> {
-                          if (t.isOwner(playerId)) {
-                            tokens.add(new JSAPIToken(t));
-                          }
-                        }));
-              }
-            }));
-
-    return tokens;
-  }
-
-  @HostAccess.Export
   public JSAPIToken getTokenByName(String tokenName) {
     boolean trusted = JSScriptEngine.inTrustedContext();
     String playerId = MapTool.getPlayer().getName();
@@ -136,24 +65,6 @@ public class JSAPITokens implements MapToolJSAPIInterface {
         if (t != null && (trusted || t.isOwner(playerId))) {
           return new JSAPIToken(t);
         }
-      }
-    }
-    return null;
-  }
-
-  @HostAccess.Export
-  public JSAPIToken getLibToken(String tokenName) throws ParserException {
-    boolean trusted = JSScriptEngine.inTrustedContext();
-    if (!trusted) {
-      throw new ParserException(I18N.getText("macro.function.general.onlyGM", "getLibToken"));
-    }
-    for (ZoneRenderer z : MapTool.getFrame().getZoneRenderers()) {
-      Token t = z.getZone().getTokenByName(tokenName);
-      if (t != null) {
-        if (t.hasOwners()) {
-          continue;
-        }
-        return new JSAPIToken(t);
       }
     }
     return null;
