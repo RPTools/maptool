8c50308a423e697faf488221a7c538e23fb590e1
Craig Wisniewski <craig.wisniewski@gmail.com>
2021-12-02 17:11:39 +1030

    start of fix

---
+++ src/main/java/net/rptools/lib/io/LegacyAsset.java
@@ -0,0 +1,35 @@
+/*
+ * This software Copyright by the RPTools.net development team, and
+ * licensed under the Affero GPL Version 3 or, at your option, any later
+ * version.
+ *
+ * MapTool Source Code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License * along with this source Code.  If not, please visit
+ * <http://www.gnu.org/licenses/> and specifically the Affero license
+ * text at <http://www.gnu.org/licenses/agpl.html>.
+ */
+package net.rptools.lib.io;
+
+import net.rptools.lib.MD5Key;
+
+public class LegacyAsset {
+  private MD5Key id;
+  private byte[] image;
+  private String name;
+
+  public MD5Key getId() {
+    return id;
+  }
+
+  public byte[] getImageData() {
+    return image;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
