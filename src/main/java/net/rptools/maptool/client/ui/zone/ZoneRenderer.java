b27b9417c97f5f25d6c75574863b77ddcf27ca8e
Alexander Youngblood <alexanderdyoungblood@gmail.com>
2022-01-14 11:55:13 -0600

    Separate BlendingComposite into own file

--- src/main/java/net/rptools/maptool/client/ui/zone/ZoneRenderer.java
+++ src/main/java/net/rptools/maptool/client/ui/zone/ZoneRenderer.java
@@ -27,12 +27,6 @@ import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.DataBuffer;
-import java.awt.image.DirectColorModel;
-import java.awt.image.Raster;
-import java.awt.image.RasterFormatException;
-import java.awt.image.WritableRaster;
 import java.text.NumberFormat;
 import java.util.*;
 import java.util.List;
@@ -61,6 +55,7 @@ import net.rptools.maptool.client.ui.htmlframe.HTMLFrameFactory;
 import net.rptools.maptool.client.ui.token.AbstractTokenOverlay;
 import net.rptools.maptool.client.ui.token.BarTokenOverlay;
 import net.rptools.maptool.client.ui.token.NewTokenDialog;
+import net.rptools.maptool.client.ui.zone.BlendingComposite;
 import net.rptools.maptool.client.walker.ZoneWalker;
 import net.rptools.maptool.language.I18N;
 import net.rptools.maptool.model.*;
@@ -1420,129 +1415,6 @@ public class ZoneRenderer extends JComponent
 
   public CodeTimer getCodeTimer() {
     return timer;
-  }
-
-  /**
-   * A custom Composite class to replace AlphaComposite for the purposes of mixing lights, auras,
-   * and other colored effects.
-   * http://www.java2s.com/Code/Java/2D-Graphics-GUI/BlendCompositeDemo.htm
-   */
-  private static class BlendingComposite implements Composite {
-
-    float srcAlphaMultiplier;
-
-    private BlendingComposite() {
-      this(1.0f);
-    }
-
-    public BlendingComposite(float srcAlphaMultiplier) {
-      this.srcAlphaMultiplier = srcAlphaMultiplier;
-    }
-
-    public static BlendingComposite getInstance() {
-      return new BlendingComposite();
-    }
-
-    public static BlendingComposite getInstance(float srcAlphaMultiplier) {
-      return new BlendingComposite(srcAlphaMultiplier);
-    }
-
-    public float getSrcAlphaMultiplier() {
-      return srcAlphaMultiplier;
-    }
-
-    private static boolean checkComponentsOrder(ColorModel cm) {
-      if (cm instanceof DirectColorModel directCM &&
-          cm.getTransferType() == DataBuffer.TYPE_INT) {
-
-        return directCM.getRedMask() == 0x00FF0000 &&
-            directCM.getGreenMask() == 0x0000FF00 &&
-            directCM.getBlueMask() == 0x000000FF &&
-            (directCM.getNumComponents() != 4 ||
-                directCM.getAlphaMask() == 0xFF000000);
-      }
-
-      return false;
-    }
-
-    @Override
-    public CompositeContext createContext(
-        ColorModel srcColorModel, ColorModel dstColorModel, RenderingHints hints) {
-      if (!checkComponentsOrder(srcColorModel) ||
-          !checkComponentsOrder(dstColorModel)) {
-        throw new RasterFormatException("Incompatible color models");
-      }
-
-      return new BlendingContext(srcAlphaMultiplier);
-    }
-  }
-
-  private static class BlendingContext implements CompositeContext {
-
-    float srcAlphaMultiplier;
-
-    public BlendingContext(float srcAlphaMultiplier) {
-      this.srcAlphaMultiplier = srcAlphaMultiplier;
-    }
-
-    @Override
-    public void compose(Raster src, Raster dstIn, WritableRaster dstOut) {
-      int w = Math.min(src.getWidth(), dstIn.getWidth());
-      int h = Math.min(src.getHeight(), dstIn.getHeight());
-
-      int[] srcRgba = new int[4];
-      int[] dstRgba = new int[4];
-      int[] newRgba = new int[4];
-      int[] srcPixels = new int[w];
-      int[] dstInPixels = new int[w];
-      int[] dstOutPixels = new int[w];
-
-      for (int y = 0; y < h; y++) {
-        src.getDataElements(0, y, w, 1, srcPixels);
-        dstIn.getDataElements(0, y, w, 1, dstInPixels);
-
-        for (int x = 0; x < w; x++) {
-          // pixels are stored as INT_ARGB
-          // our arrays are [R, G, B, A]
-          int pixel = srcPixels[x];
-          srcRgba[0] = (pixel >> 16) & 0xFF;
-          srcRgba[1] = (pixel >> 8) & 0xFF;
-          srcRgba[2] = (pixel) & 0xFF;
-          srcRgba[3] = (pixel >> 24) & 0xFF;
-
-          pixel = dstInPixels[x];
-          dstRgba[0] = (pixel >> 16) & 0xFF;
-          dstRgba[1] = (pixel >> 8) & 0xFF;
-          dstRgba[2] = (pixel) & 0xFF;
-          dstRgba[3] = (pixel >> 24) & 0xFF;
-
-          // Combine colors appropriately
-          srcRgba[3] = (int) (srcRgba[3] * srcAlphaMultiplier);
-
-          newRgba[0] = Math.min(srcRgba[0] + dstRgba[0], 255);
-          newRgba[1] = Math.min(srcRgba[1] + dstRgba[1], 255);
-          newRgba[2] = Math.min(srcRgba[2] + dstRgba[2], 255);
-
-          if (dstRgba[3] == 0) {
-            newRgba[3] = srcRgba[3];
-          } else {
-            newRgba[3] = Math.min(srcRgba[3], dstRgba[3]);
-          }
-
-          // Recombine [R, G, B, A] array to INT_ARGB
-          dstOutPixels[x] =
-              (newRgba[3] & 0xFF) << 24
-                  | (newRgba[0] & 0xFF) << 16
-                  | (newRgba[1] & 0xFF) << 8
-                  | newRgba[2] & 0xFF;
-        }
-
-        dstOut.setDataElements(0, y, w, 1, dstOutPixels);
-      }
-    }
-
-    @Override
-    public void dispose() {}
   }
 
   /** Map of the lights from drawableLightCache that have been combined. */
