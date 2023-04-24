4e714e1cc9dbd7a11ea8cec32b29a3393262b15b
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2023-04-24 12:49:34 -0700

    Fix package names

--- src/main/java/net/rptools/maptool/client/ui/token/dialog/edit/TokenTopologyPanel.java
+++ src/main/java/net/rptools/maptool/client/ui/token/dialog/edit/TokenTopologyPanel.java
@@ -12,7 +12,7 @@
  * <http://www.gnu.org/licenses/> and specifically the Affero license
  * text at <http://www.gnu.org/licenses/agpl.html>.
  */
-package net.rptools.maptool.client.ui.token.edit;
+package net.rptools.maptool.client.ui.token.dialog.edit;
 
 import java.awt.Color;
 import java.awt.Cursor;
