cf76bbfbce7c92a055419795a48a5650514e0469
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2023-08-28 13:51:17 -0700

    Fix comments about ordering of lumens levels to avoid confusion

--- src/main/java/net/rptools/maptool/client/ui/zone/ZoneRenderer.java
+++ src/main/java/net/rptools/maptool/client/ui/zone/ZoneRenderer.java
@@ -1516,9 +1516,6 @@ public class ZoneRenderer extends JComponent
       timer.stop("renderLumensOverlay:setTransform");
 
       timer.start("renderLumensOverlay:drawLumens");
-      // Lumens are ordered to be weak to strong. That works for us as we will draw the stronger
-      // areas overtop the weaker areas using `AlphaComposite.Src` to make sure the stronger one
-      // "wins".
       for (final var lumensLevel : disjointLumensLevels) {
         final var lumensStrength = lumensLevel.lumensStrength();
 
