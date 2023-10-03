2412e9f789f8b1a9faf5801f8ec1ae3ac02e490f
jmr3366 <jmr3366@hotmail.com>
2023-10-03 11:34:54 -0400

    onMouseOver event

---
+++ src/main/java/net/rptools/maptool/events/TokenHoverListener.java
@@ -0,0 +1,90 @@
+/*
+ * This software Copyright by the RPTools.net development team, and
+ * licensed under the Affero GPL Version 3 or, at your option, any later
+ * version.
+ *
+ * MapTool Source Code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License * along with this source Code.  If not, please visit
+ * <http://www.gnu.org/licenses/> and specifically the Affero license
+ * text at <http://www.gnu.org/licenses/agpl.html>.
+ */
+package net.rptools.maptool.events;
+
+import com.google.common.eventbus.Subscribe;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+import net.rptools.maptool.client.events.TokenHoverEnter;
+import net.rptools.maptool.client.events.TokenHoverExit;
+import net.rptools.maptool.language.I18N;
+import net.rptools.maptool.model.library.Library;
+import net.rptools.maptool.model.library.LibraryManager;
+import net.rptools.maptool.util.EventMacroUtil;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class TokenHoverListener {
+  public static final String ON_MOUSE_OVER_CALLBACK = "onMouseOver";
+  private static final Logger LOGGER = LogManager.getLogger(EventMacroUtil.class);
+
+  public TokenHoverListener() {}
+
+  @Subscribe
+  public void onMouseOverEnter(TokenHoverEnter event) {
+    var token = event.token();
+    var tokX = event.token().getX();
+    var tokY = event.token().getY();
+    var shiftKey = event.shiftDown();
+    var ctrlKey = event.controlDown();
+    try {
+      var libs = new LibraryManager().getLegacyEventTargets(ON_MOUSE_OVER_CALLBACK).get();
+      if (!libs.isEmpty()) {
+        for (Library handler : libs) {
+          try {
+            String libraryNamespace = handler.getNamespace().get();
+            EventMacroUtil.callEventHandler(
+                ON_MOUSE_OVER_CALLBACK,
+                libraryNamespace,
+                token.getId().toString() + "," + tokX + "," + tokY + "," + shiftKey + "," + ctrlKey,
+                null,
+                Collections.emptyMap(),
+                false);
+          } catch (InterruptedException | ExecutionException e) {
+            throw new AssertionError("Error retrieving library namespace");
+          }
+        }
+      }
+    } catch (InterruptedException | ExecutionException e) {
+      LOGGER.error(I18N.getText("library.error.retrievingEventHandler"), e);
+    }
+  }
+
+  @Subscribe
+  public void onMouseOverExit(TokenHoverExit event) {
+    var token = event.token();
+    try {
+      var libs = new LibraryManager().getLegacyEventTargets(ON_MOUSE_OVER_CALLBACK).get();
+      if (!libs.isEmpty()) {
+        for (Library handler : libs) {
+          try {
+            String libraryNamespace = handler.getNamespace().get();
+            EventMacroUtil.callEventHandler(
+                ON_MOUSE_OVER_CALLBACK,
+                libraryNamespace,
+                token.getId().toString() + ",exit",
+                null,
+                Collections.emptyMap(),
+                false);
+          } catch (InterruptedException | ExecutionException e) {
+            throw new AssertionError("Error retrieving library namespace");
+          }
+        }
+      }
+    } catch (InterruptedException | ExecutionException e) {
+      LOGGER.error(I18N.getText("library.error.retrievingEventHandler"), e);
+    }
+  }
+}
