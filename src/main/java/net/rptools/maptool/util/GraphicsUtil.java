e4df2255848620e0e60bc9b401ab6020e7cf0c07
bubblobill <45483160+bubblobill@users.noreply.github.com>
2023-11-19 23:05:36 +0800

    renamed utility method getPoint to getPointAtVector

--- src/main/java/net/rptools/maptool/util/GraphicsUtil.java
+++ src/main/java/net/rptools/maptool/util/GraphicsUtil.java
@@ -394,8 +394,8 @@ public class GraphicsUtil {
       double topAngle = bottomAngle + 180;
       // System.out.println(angle + " - " + delta + " - " + bottomAngle + " - " + topAngle);
 
-      bottomList.add(getPoint(points[i], bottomAngle, width));
-      topList.add(getPoint(points[i], topAngle, width));
+      bottomList.add(getPointAtVector(points[i], bottomAngle, width));
+      topList.add(getPointAtVector(points[i], topAngle, width));
     }
     // System.out.println(bottomList);
     // System.out.println(topList);
@@ -415,7 +415,7 @@ public class GraphicsUtil {
     return new Area(path);
   }
 
-  private static Point2D getPoint(Point2D point, double angle, double length) {
+  private static Point2D getPointAtVector(Point2D point, double angle, double length) {
     double x = point.getX() + length * Math.cos(Math.toRadians(angle));
     double y = point.getY() - length * Math.sin(Math.toRadians(angle));
 
