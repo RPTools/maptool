ad70c259cbb313e1f1e64c48f176acda8f9a4b22
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2024-04-12 12:10:08 -0700

    Remove renderer from BlastTemplate
    This renderer was still in use, but it adds some unnecessary constraints to BlastTemplate. Mainly the issue is that it
    had to modify the renderer's shape, and had to override `draw()`/`drawBackground()` instead of the `paint()` method
    whichi is more idiomatic for templates.
    
    Now we don't even bother keeping a shape up-to-date as we just create a new rectangle whenever it is needed. This is not
    expensive at all, especially compared to what some other templates have to do.

--- src/main/java/net/rptools/maptool/model/drawing/BlastTemplate.java
+++ src/main/java/net/rptools/maptool/model/drawing/BlastTemplate.java
@@ -15,17 +15,13 @@
 package net.rptools.maptool.model.drawing;
 
 import com.google.protobuf.StringValue;
-import java.awt.AlphaComposite;
-import java.awt.Composite;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.Shape;
+import java.awt.RenderingHints;
 import java.awt.geom.Area;
 import javax.annotation.Nonnull;
-import net.rptools.maptool.client.MapTool;
 import net.rptools.maptool.model.GUID;
 import net.rptools.maptool.model.Zone;
-import net.rptools.maptool.model.ZonePoint;
 import net.rptools.maptool.server.proto.drawing.BlastTemplateDto;
 import net.rptools.maptool.server.proto.drawing.DrawableDto;
 
@@ -40,9 +36,6 @@ public class BlastTemplate extends ConeTemplate {
   /*---------------------------------------------------------------------------------------------
    * Instance Variables
    *-------------------------------------------------------------------------------------------*/
-
-  /** Renderer for the blast. The {@link Shape} is just a rectangle. */
-  private ShapeDrawable renderer = new ShapeDrawable(new Rectangle());
 
   private int offsetX;
   private int offsetY;
@@ -59,28 +52,16 @@ public class BlastTemplate extends ConeTemplate {
    * Instance Methods
    *-------------------------------------------------------------------------------------------*/
 
-  /**
-   * This methods adjusts the rectangle in the renderer to match the new radius, vertex, or
-   * location.
-   */
-  private void adjustRectangle() {
-    if (getZoneId() == null) return;
-    Zone zone;
-    if (MapTool.isHostingServer()) {
-      zone = MapTool.getServer().getCampaign().getZone(getZoneId());
-    } else {
-      zone = MapTool.getCampaign().getZone(getZoneId());
+  private Rectangle makeShape(Zone zone) {
+    if (zone == null) {
+      return new Rectangle();
     }
-    if (zone == null) return;
 
     int gridSize = zone.getGrid().getSize();
     int size = getRadius() * gridSize;
 
-    Rectangle r = (Rectangle) renderer.getShape();
-    r.setBounds(getVertex().x, getVertex().y, size, size);
-
-    r.x += offsetX * gridSize;
-    r.y += offsetY * gridSize;
+    return new Rectangle(
+        getVertex().x + offsetX * gridSize, getVertex().y + offsetY * gridSize, size, size);
   }
 
   /*---------------------------------------------------------------------------------------------
@@ -89,7 +70,7 @@ public class BlastTemplate extends ConeTemplate {
 
   @Override
   public Rectangle getBounds(Zone zone) {
-    Rectangle r = new Rectangle(renderer.getShape().getBounds());
+    Rectangle r = makeShape(zone);
     // We don't know pen width, so add some padding to account for it
     r.x -= 5;
     r.y -= 5;
@@ -136,17 +117,6 @@ public class BlastTemplate extends ConeTemplate {
       }
       offsetX = centerOffset + Math.min(Math.max(lowerBound, relX), upperBound);
     }
-    adjustRectangle();
-  }
-
-  /**
-   * @see
-   *     net.rptools.maptool.model.drawing.AbstractTemplate#setVertex(net.rptools.maptool.model.ZonePoint)
-   */
-  @Override
-  public void setVertex(ZonePoint vertex) {
-    super.setVertex(vertex);
-    adjustRectangle();
   }
 
   /**
@@ -162,22 +132,26 @@ public class BlastTemplate extends ConeTemplate {
    *-------------------------------------------------------------------------------------------*/
 
   @Override
-  protected void draw(Zone zone, Graphics2D g) {
-    renderer.draw(zone, g);
-  }
+  protected void paint(Zone zone, Graphics2D g, boolean border, boolean area) {
+    Object oldAA = g.getRenderingHint(RenderingHints.KEY_ANTIALIASING);
+    try {
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-  @Override
-  protected void drawBackground(Zone zone, Graphics2D g) {
-    Composite old = g.getComposite();
-    if (old != AlphaComposite.Clear)
-      g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, DEFAULT_BG_ALPHA));
-    renderer.drawBackground(zone, g);
-    g.setComposite(old);
+      var shape = makeShape(zone);
+      if (area) {
+        g.fill(shape);
+      }
+      if (border) {
+        g.draw(shape);
+      }
+    } finally {
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, oldAA);
+    }
   }
 
   @Override
   public @Nonnull Area getArea(Zone zone) {
-    return renderer.getArea(zone);
+    return new Area(makeShape(zone));
   }
 
   public int getOffsetX() {
