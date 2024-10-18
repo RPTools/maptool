2b9b98b8890121222bc29f32074a7a8b41584a57
jmr3366 <jmr3366@hotmail.com>
2024-10-18 08:24:03 -0400

    #4961 NPE on mouse off (needs testing, I can't validate) #4954 User Overlays break with new statsheets

--- src/main/java/net/rptools/maptool/client/ui/sheet/stats/StatSheet.java
+++ src/main/java/net/rptools/maptool/client/ui/sheet/stats/StatSheet.java
@@ -17,7 +17,6 @@ package net.rptools.maptool.client.ui.sheet.stats;
 import java.io.IOException;
 import java.net.URL;
 import javafx.application.Platform;
-import net.rptools.maptool.client.AppConstants;
 import net.rptools.maptool.client.MapTool;
 import net.rptools.maptool.model.Token;
 import net.rptools.maptool.model.sheet.stats.StatSheetContext;
@@ -51,15 +50,15 @@ public class StatSheet {
             var overlay =
                 MapTool.getFrame()
                     .getOverlayPanel()
-                    .getOverlay(AppConstants.INTERNAL_MAP_UNDER_POINTER_HTML_OVERLAY_NAME);
+                    .getOverlay("MapTool Internal Map Under Pointer Overlay");
             if (overlay != null) {
               overlay.updateContents(output, true);
             } else {
               MapTool.getFrame()
                   .getOverlayPanel()
                   .showOverlay(
-                      AppConstants.INTERNAL_MAP_UNDER_POINTER_HTML_OVERLAY_NAME,
-                      Integer.MIN_VALUE,
+                      "MapTool Internal Map Under Pointer Overlay",
+                      Integer.MAX_VALUE,
                       output,
                       null);
             }
@@ -76,8 +75,10 @@ public class StatSheet {
           var overlay =
               MapTool.getFrame()
                   .getOverlayPanel()
-                  .getOverlay(AppConstants.INTERNAL_MAP_UNDER_POINTER_HTML_OVERLAY_NAME);
-          overlay.updateContents("", true);
+                  .getOverlay("MapTool Internal Map Under Pointer Overlay");
+          if (overlay != null) {
+            overlay.updateContents("", true);
+          }
         });
   }
 }
