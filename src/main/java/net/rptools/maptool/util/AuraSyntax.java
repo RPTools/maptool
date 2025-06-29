f1179bfa1c696123d60b4e0d3c11665e861badeb
bubblobill <45483160+bubblobill@users.noreply.github.com>
2025-06-30 01:37:02 +0800

    missed this

--- src/main/java/net/rptools/maptool/util/AuraSyntax.java
+++ src/main/java/net/rptools/maptool/util/AuraSyntax.java
@@ -372,7 +372,7 @@ public class AuraSyntax {
     }
 
     return LightSource.createRegular(
-        name, id, LightSource.Type.AURA, scaleWithToken, ignoresVBL, lights);
+        name, null, id, LightSource.Type.AURA, scaleWithToken, ignoresVBL, lights);
   }
 
   private String toHex(Color color) {
