1b097c8a72bba5f780e3bd9a725989eda3cc18f2
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2026-08-20 07:57:33 -0700

    Remove aberrant license headers

--- src/main/java/net/rptools/maptool/client/functions/MathFunctions.java
+++ src/main/java/net/rptools/maptool/client/functions/MathFunctions.java
@@ -33,14 +33,6 @@ import net.rptools.parser.VariableResolver;
 import net.rptools.parser.function.AbstractFunction;
 import net.rptools.parser.function.ParameterException;
 
-/*
- * This software Copyright by the RPTools.net development team, and licensed under the GPL Version 3 or, at your option, any later version.
- *
- * MapTool 2 Source Code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this source Code. If not, see <http://www.gnu.org/licenses/>
- */
 public class MathFunctions extends AbstractFunction {
 
   private static MathFunctions instance = new MathFunctions();
